68b65910cda4fc008b6d40941b8ed0ba4c187666
Laura <laura.hornholt@specialisterne.com>
2025-10-02 12:23:43 +0200

    Changed some globalization

--- CerealAPI/CerealFactory.cs
+++ CerealAPI/CerealFactory.cs
@@ -1,4 +1,6 @@
-﻿namespace CerealAPI;
+﻿using System.Globalization;
+
+namespace CerealAPI;
 
 public class CerealFactory
 {
@@ -13,15 +15,15 @@ public class CerealFactory
             Protein = int.Parse(fields[4]),
             Fat = int.Parse(fields[5]),
             Sodium = int.Parse(fields[6]),
-            Fiber = float.Parse(fields[7].Replace(",", ".")),
-            Carbo = float.Parse(fields[8].Replace(",", ".")),
+            Fiber = float.Parse(fields[7].Replace(",", "."), CultureInfo.InvariantCulture),
+            Carbo = float.Parse(fields[8].Replace(",", "."), CultureInfo.InvariantCulture),
             Sugars = int.Parse(fields[9]),
             Potass = int.Parse(fields[10]),
             Vitamins = int.Parse(fields[11]),
             Shelf = int.Parse(fields[12]),
-            Weight = float.Parse(fields[13].Replace(",", ".")),
-            Cups = float.Parse(fields[14].Replace(",", ".")),
-            Rating = float.Parse(fields[15].Replace(",", "."))
+            Weight = float.Parse(fields[13].Replace(",", "."), CultureInfo.InvariantCulture),
+            Cups = float.Parse(fields[14].Replace(",", "."), CultureInfo.InvariantCulture),
+            Rating = float.Parse(fields[15].Replace(",", "."), CultureInfo.InvariantCulture)
         };
     }
 }
